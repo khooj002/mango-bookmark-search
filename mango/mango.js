e6fca223a7f94d4ec17a4c82a0e9ad1ad303ad9c
Justin Khoo <jkhoo002@ucr.edu>
2015-02-21 17:09:33 -0800

    added comments

--- mango/mango.js
+++ mango/mango.js
@@ -1,13 +1,17 @@
+ // intialize controller 
  var mangolar = angular.module("mangolar", []);
  //var bookmarksnodes;
  mangolar.controller('MangoCtrl', ['$scope', function($scope) {
  	$scope.updateText=function(text2){
+ 		//call chrome bookmarks search libray to get the user's bookmarks
+ 		//received in the form of array
  		chrome.bookmarks.search(text2, function(myBookmarks){
  			// for(i = 0; i < myBookmarks.length; i++){
  			// 	//	var id = myBookmarks[0].id;
  			// 	 var id = myBookmarks[i].id;
  				 // chrome.bookmarks.get(id, function(bookmark){
  				 // 	var url = bookmark[i].url;
+ 				 // console.log(bookmarku[i]);
  				 // });
  				$scope.bookmarksnodes = myBookmarks;
  			
